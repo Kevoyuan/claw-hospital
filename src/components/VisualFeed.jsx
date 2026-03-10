39d63469a530e1faa6013ad6502815e4e9b9ed4e
Kevoyuan <kevinsyuan@gmail.com>
2026-03-10 23:13:30 +0100

    React Hook

--- src/components/VisualFeed.jsx
+++ src/components/VisualFeed.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Import all Digimon Pixel Art Assets
 import clinicImg from '../assets/clinic-isometric-small.png';
